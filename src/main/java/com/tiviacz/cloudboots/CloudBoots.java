431951feb6afe04b6e946fa9a477fd1f49bf0e02
Tiviacz1337 <prusek.kamil@o2.pl>
2018-11-28 18:00:22 +0100

    Gradle properties

--- src/main/java/com/tiviacz/cloudboots/CloudBoots.java
+++ src/main/java/com/tiviacz/cloudboots/CloudBoots.java
@@ -19,7 +19,7 @@ public class CloudBoots
 {	
 	public static final String MODID = "cloudboots";
 	public static final String NAME = "Cloud Boots Mod";
-	public static final String VERSION = "1.3.1";
+	public static final String VERSION = "1.3.2";
 
 	public static final Item CLOUD_BOOTS = new ItemCloudBoots();
 	public static final Item GOLDEN_FEATHER = new ItemGoldenFeather();
