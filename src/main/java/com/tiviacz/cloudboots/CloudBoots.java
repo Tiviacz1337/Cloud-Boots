1cd6c10b038698f13323218fb75985e6077e9eef
Tiviacz1337 <prusek.kamil@o2.pl>
2019-01-30 20:56:12 +0100

    Fixed particles not visible by other players

--- src/main/java/com/tiviacz/cloudboots/CloudBoots.java
+++ src/main/java/com/tiviacz/cloudboots/CloudBoots.java
@@ -1,14 +1,10 @@
 package com.tiviacz.cloudboots;
 
 import net.minecraft.client.renderer.block.model.ModelResourceLocation;
-import net.minecraft.entity.player.EntityPlayer;
-import net.minecraft.inventory.EntityEquipmentSlot;
 import net.minecraft.item.Item;
-import net.minecraft.item.ItemStack;
 import net.minecraftforge.client.event.ModelRegistryEvent;
 import net.minecraftforge.client.model.ModelLoader;
 import net.minecraftforge.event.RegistryEvent.Register;
-import net.minecraftforge.event.entity.living.LivingFallEvent;
 import net.minecraftforge.fml.common.Mod;
 import net.minecraftforge.fml.common.Mod.EventBusSubscriber;
 import net.minecraftforge.fml.common.eventhandler.SubscribeEvent;
@@ -19,7 +15,7 @@ public class CloudBoots
 {	
 	public static final String MODID = "cloudboots";
 	public static final String NAME = "Cloud Boots Mod";
-	public static final String VERSION = "1.3.2";
+	public static final String VERSION = "1.4.1";
 
 	public static final Item CLOUD_BOOTS = new ItemCloudBoots();
 	public static final Item GOLDEN_FEATHER = new ItemGoldenFeather();
@@ -36,21 +32,5 @@ public class CloudBoots
 	{
 		ModelLoader.setCustomModelResourceLocation(CLOUD_BOOTS, 0, new ModelResourceLocation(CLOUD_BOOTS.getRegistryName(), "inventory"));	
 		ModelLoader.setCustomModelResourceLocation(GOLDEN_FEATHER, 0, new ModelResourceLocation(GOLDEN_FEATHER.getRegistryName(), "inventory"));	
-	}
-	
-	@SubscribeEvent
-	public static void cancelPlayerFallDamage(LivingFallEvent event)
-	{
-		if(event.getEntity() instanceof EntityPlayer)
-		{
-			EntityPlayer player = (EntityPlayer) event.getEntity();
-			ItemStack helditem = player.getHeldItemMainhand();
-			ItemStack helditemoff = player.getHeldItemOffhand();
-			
-			if(player.getItemStackFromSlot(EntityEquipmentSlot.FEET).getItem() == CLOUD_BOOTS || helditem.getItem() == GOLDEN_FEATHER || helditemoff.getItem() == GOLDEN_FEATHER)
-			{
-				event.setDistance(0.0F);
-			}
-		}
 	}
 }
