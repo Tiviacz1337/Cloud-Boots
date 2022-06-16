b2222874b1491fa1b8857dbb7a87743e57d055c6
Tiviacz1337 <34790800+Tiviacz1337@users.noreply.github.com>
2022-06-17 00:31:18 +0200

    misc

--- src/main/java/com/tiviacz/cloudboots/CloudBootsItem.java
+++ src/main/java/com/tiviacz/cloudboots/CloudBootsItem.java
@@ -2,26 +2,25 @@ package com.tiviacz.cloudboots;
 
 import com.google.common.collect.ImmutableMultimap;
 import com.google.common.collect.Multimap;
-import net.minecraft.entity.Entity;
-import net.minecraft.entity.ai.attributes.Attribute;
-import net.minecraft.entity.ai.attributes.AttributeModifier;
-import net.minecraft.entity.ai.attributes.AttributeModifier.Operation;
-import net.minecraft.entity.ai.attributes.Attributes;
-import net.minecraft.entity.player.PlayerEntity;
-import net.minecraft.inventory.EquipmentSlotType;
-import net.minecraft.item.ArmorItem;
-import net.minecraft.item.IArmorMaterial;
-import net.minecraft.item.ItemStack;
-import net.minecraft.item.Items;
-import net.minecraft.item.crafting.Ingredient;
-import net.minecraft.particles.ParticleTypes;
-import net.minecraft.potion.EffectInstance;
-import net.minecraft.potion.Effects;
-import net.minecraft.util.LazyValue;
-import net.minecraft.util.SoundEvent;
-import net.minecraft.util.SoundEvents;
-import net.minecraft.world.World;
-import net.minecraft.world.server.ServerWorld;
+import net.minecraft.core.particles.ParticleTypes;
+import net.minecraft.server.level.ServerLevel;
+import net.minecraft.sounds.SoundEvent;
+import net.minecraft.sounds.SoundEvents;
+import net.minecraft.util.LazyLoadedValue;
+import net.minecraft.world.effect.MobEffectInstance;
+import net.minecraft.world.effect.MobEffects;
+import net.minecraft.world.entity.Entity;
+import net.minecraft.world.entity.EquipmentSlot;
+import net.minecraft.world.entity.ai.attributes.Attribute;
+import net.minecraft.world.entity.ai.attributes.AttributeModifier;
+import net.minecraft.world.entity.ai.attributes.Attributes;
+import net.minecraft.world.entity.player.Player;
+import net.minecraft.world.item.ArmorItem;
+import net.minecraft.world.item.ArmorMaterial;
+import net.minecraft.world.item.ItemStack;
+import net.minecraft.world.item.Items;
+import net.minecraft.world.item.crafting.Ingredient;
+import net.minecraft.world.level.Level;
 
 public class CloudBootsItem extends ArmorItem
 {
@@ -29,41 +28,47 @@ public class CloudBootsItem extends ArmorItem
 	
 	public CloudBootsItem(Properties builder) 
 	{
-		super(CloudArmorMaterial.CLOUD, EquipmentSlotType.FEET, builder);
+		super(CloudArmorMaterial.CLOUD, EquipmentSlot.FEET, builder);
 
-		Multimap<Attribute, AttributeModifier> attributeMap = getAttributeModifiers(EquipmentSlotType.FEET);
+		Multimap<Attribute, AttributeModifier> attributeMap = getDefaultAttributeModifiers(EquipmentSlot.FEET);
 		ImmutableMultimap.Builder<Attribute, AttributeModifier> modifierBuilder = ImmutableMultimap.builder();
 		modifierBuilder.putAll(attributeMap);
-		modifierBuilder.put(Attributes.MOVEMENT_SPEED, new AttributeModifier("CloudBootsMovementSpeedModifier", CloudBootsConfigurator.bonusSpeed, Operation.MULTIPLY_TOTAL));
+		modifierBuilder.put(Attributes.MOVEMENT_SPEED, new AttributeModifier("CloudBootsMovementSpeedModifier", 0.15D, AttributeModifier.Operation.MULTIPLY_TOTAL));
 		this.attributeModifier = modifierBuilder.build();
+	}
+
+	@Override
+	public ArmorMaterial getMaterial()
+	{
+		return CloudArmorMaterial.CLOUD;
 	}
 	
 	@Override
-	public Multimap<Attribute, AttributeModifier> getAttributeModifiers(EquipmentSlotType slot, ItemStack stack)
+	public Multimap<Attribute, AttributeModifier> getAttributeModifiers(EquipmentSlot slot, ItemStack stack)
     {
-		return slot == EquipmentSlotType.FEET ? this.attributeModifier : super.getAttributeModifiers(slot, stack);
+		return slot == EquipmentSlot.FEET ? this.attributeModifier : super.getAttributeModifiers(slot, stack);
     }
 			
 	@Override
-	public void inventoryTick(ItemStack stack, World worldIn, Entity entityIn, int itemSlot, boolean isSelected) 
+	public void inventoryTick(ItemStack stack, Level level, Entity entityIn, int itemSlot, boolean isSelected)
 	{
-		if(entityIn instanceof PlayerEntity)
+		if(entityIn instanceof Player)
 		{
-			PlayerEntity player = (PlayerEntity)entityIn;
+			Player player = (Player)entityIn;
 			
-			if(player.getItemStackFromSlot(EquipmentSlotType.FEET).getItem() == this)
+			if(player.getItemBySlot(EquipmentSlot.FEET).getItem() == this)
 			{
-				player.addPotionEffect(new EffectInstance(Effects.JUMP_BOOST, 0, CloudBootsConfigurator.jumpBoost, false, false));
+				player.addEffect(new MobEffectInstance(MobEffects.JUMP, 0, 4, false, false));
 				
 				if(!player.isOnGround())
 				{
-					player.jumpMovementFactor += CloudBootsConfigurator.jumpMovementFactor;
+					player.flyingSpeed += 0.012D;
 					
 					if(player.fallDistance >= 1.0F)
 					{
-						if(!worldIn.isRemote && worldIn instanceof ServerWorld && CloudBootsConfigurator.spawnParticles)
+						if(!level.isClientSide && level instanceof ServerLevel)
 						{
-							((ServerWorld)worldIn).spawnParticle(ParticleTypes.CLOUD, player.lastTickPosX, player.lastTickPosY, player.lastTickPosZ, 3, 0, 0, 0, (random.nextFloat() - 0.5F));
+							((ServerLevel)level).sendParticles(ParticleTypes.CLOUD, player.xo, player.yo, player.zo, 3, 0, 0, 0, (level.random.nextFloat() - 0.5F));
 						}
 						
 					/*	else if(world.isRemote)
@@ -73,19 +78,15 @@ public class CloudBootsItem extends ArmorItem
 								world.spawnParticle(EnumParticleTypes.CLOUD, player.posX, player.posY, player.posZ, (itemRand.nextFloat() - 0.5F), -0.5D, (itemRand.nextFloat() - 0.5F));
 							}
 						} */
-
-						if(!CloudBootsConfigurator.fallDamage)
-						{
-							player.fallDistance = 0F;
-						}
+						player.fallDistance = 0F;
 					}
 				}
 				
 				if(player.isSprinting())
 				{
-					if(!worldIn.isRemote && worldIn instanceof ServerWorld && CloudBootsConfigurator.spawnParticles)
+					if(!level.isClientSide && level instanceof ServerLevel)
 					{
-						((ServerWorld)worldIn).spawnParticle(ParticleTypes.CLOUD, player.lastTickPosX, player.lastTickPosY, player.lastTickPosZ, 1, 0, 0, 0, (random.nextFloat() - 0.5F));
+						((ServerLevel)level).sendParticles(ParticleTypes.CLOUD, player.xo, player.yo, player.zo, 1, 0, 0, 0, (level.random.nextFloat() - 0.5F));
 					}
 	
 				/*	else if(world.isRemote)
@@ -98,43 +99,43 @@ public class CloudBootsItem extends ArmorItem
 	}
 	
 	@Override
-	public boolean getIsRepairable(ItemStack toRepair, ItemStack repair)
+	public boolean isValidRepairItem(ItemStack toRepair, ItemStack repair)
 	{
-		return super.getIsRepairable(toRepair, repair);
+		return super.isValidRepairItem(toRepair, repair);
 	}
 
-	public static class CloudArmorMaterial implements IArmorMaterial
+	public static class CloudArmorMaterial implements ArmorMaterial
 	{
 		public static final CloudArmorMaterial CLOUD = new CloudArmorMaterial();
 		
 		@Override
-		public int getDurability(EquipmentSlotType slotIn) 
+		public int getDurabilityForSlot(EquipmentSlot slotIn)
 		{
-			return CloudBootsConfigurator.durability;
+			return 1337;
 		}
 
 		@Override
-		public int getDamageReductionAmount(EquipmentSlotType slotIn) 
+		public int getDefenseForSlot(EquipmentSlot slotIn)
 		{
-			return CloudBootsConfigurator.damageReductionAmount;
+			return 3;
 		}
 
 		@Override
-		public int getEnchantability() 
+		public int getEnchantmentValue()
 		{
-			return CloudBootsConfigurator.enchantability;
+			return 10;
 		}
 
 		@Override
-		public SoundEvent getSoundEvent() 
+		public SoundEvent getEquipSound()
 		{
-			return SoundEvents.ITEM_ARMOR_EQUIP_DIAMOND;
+			return SoundEvents.ARMOR_EQUIP_DIAMOND;
 		}
 
 		@Override
-		public Ingredient getRepairMaterial() 
+		public Ingredient getRepairIngredient()
 		{
-			return new LazyValue<>(() -> Ingredient.fromItems(Items.GOLD_INGOT, CloudBoots.ModItems.GOLDEN_FEATHER.get())).getValue();
+			return new LazyLoadedValue<>(() -> Ingredient.of(Items.GOLD_INGOT, CloudBoots.ModItems.GOLDEN_FEATHER.get())).get();
 		}
 
 		@Override
@@ -146,13 +147,13 @@ public class CloudBootsItem extends ArmorItem
 		@Override
 		public float getToughness() 
 		{
-			return CloudBootsConfigurator.toughness;
+			return 2.0F;
 		}
 
 		@Override
 		public float getKnockbackResistance()
 		{
-			return CloudBootsConfigurator.knockbackResistance;
+			return 0.0F;
 		}
 	}
 }
