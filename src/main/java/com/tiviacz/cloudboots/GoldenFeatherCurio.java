601e46243a1aa4cabdf9e2ef6352695595a8a477
Tiviacz1337 <34790800+Tiviacz1337@users.noreply.github.com>
2022-06-17 00:28:16 +0200

    curio compat

---
+++ src/main/java/com/tiviacz/cloudboots/GoldenFeatherCurio.java
@@ -0,0 +1,46 @@
+package com.tiviacz.cloudboots;
+
+import net.minecraft.entity.LivingEntity;
+import net.minecraft.entity.player.ServerPlayerEntity;
+import net.minecraft.item.ItemStack;
+import net.minecraft.particles.ParticleTypes;
+import net.minecraft.world.server.ServerWorld;
+import top.theillusivec4.curios.api.type.capability.ICurio;
+
+public class GoldenFeatherCurio implements ICurio
+{
+    private final ItemStack stack;
+
+    public static ICurio createGoldenFeatherCurioProvider(ItemStack stack)
+    {
+        return new GoldenFeatherCurio(stack);
+    }
+
+    public GoldenFeatherCurio(ItemStack stack)
+    {
+        this.stack = stack;
+    }
+
+    @Override
+    public void curioTick(String identifier, int index, LivingEntity livingEntity)
+    {
+        if(livingEntity instanceof ServerPlayerEntity)
+        {
+            if(livingEntity.fallDistance >= 3.0F)
+            {
+                stack.hurt(1, livingEntity.level.random, (ServerPlayerEntity)livingEntity);
+                livingEntity.fallDistance = 0.0F;
+
+                if(!livingEntity.level.isClientSide && livingEntity.level instanceof ServerWorld)
+                {
+                    ((ServerWorld)livingEntity.level).sendParticles(ParticleTypes.CLOUD, livingEntity.xOld, livingEntity.yOld, livingEntity.zOld, 3, 0, 0, 0, (livingEntity.level.random.nextFloat() - 0.5F));
+                }
+
+		/*		for(int i = 0; i < 3; i++)
+				{
+					worldIn.spawnParticle(EnumParticleTypes.CLOUD, entityIn.posX, entityIn.posY, entityIn.posZ, (itemRand.nextFloat() - 0.5F), -0.5D, (itemRand.nextFloat() - 0.5F));
+				}   */
+            }
+        }
+    }
+}
