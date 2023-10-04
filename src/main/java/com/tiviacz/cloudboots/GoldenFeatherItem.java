17a48ab863e4978406d52c81a3f2da3d32820060
Tiviacz1337 <34790800+Tiviacz1337@users.noreply.github.com>
2023-10-04 15:49:44 +0200

    🐛 Fixed golden feather bugs

--- src/main/java/com/tiviacz/cloudboots/GoldenFeatherItem.java
+++ src/main/java/com/tiviacz/cloudboots/GoldenFeatherItem.java
@@ -1,12 +1,14 @@
 package com.tiviacz.cloudboots;
 
 import net.minecraft.entity.Entity;
+import net.minecraft.entity.EquipmentSlot;
 import net.minecraft.item.Item;
 import net.minecraft.item.ItemStack;
 import net.minecraft.item.Items;
 import net.minecraft.particle.ParticleTypes;
 import net.minecraft.server.network.ServerPlayerEntity;
 import net.minecraft.server.world.ServerWorld;
+import net.minecraft.util.Hand;
 import net.minecraft.world.World;
 
 public class GoldenFeatherItem extends Item
@@ -19,16 +21,33 @@ public class GoldenFeatherItem extends Item
     @Override
     public void inventoryTick(ItemStack stack, World worldIn, Entity entityIn, int itemSlot, boolean isSelected)
     {
-        if(isSelected && entityIn instanceof ServerPlayerEntity serverPlayer)
+        if(entityIn instanceof ServerPlayerEntity)
         {
-            if(serverPlayer.fallDistance >= 3.0F)
+            if(((ServerPlayerEntity)entityIn).getStackInHand(Hand.MAIN_HAND).getItem() == stack.getItem())
             {
-                stack.damage(1, worldIn.random, serverPlayer);
-                serverPlayer.fallDistance = 0.0F;
-
-                if(!worldIn.isClient && worldIn instanceof ServerWorld serverWorld)
+                if(entityIn.fallDistance >= 3.0F)
                 {
-                    serverWorld.spawnParticles(ParticleTypes.CLOUD, serverPlayer.lastRenderX, serverPlayer.lastRenderY, serverPlayer.lastRenderZ, 3, 0, 0, 0, (worldIn.random.nextFloat() - 0.5F));
+                    ((ServerPlayerEntity)entityIn).getStackInHand(Hand.MAIN_HAND).damage(1, (ServerPlayerEntity)entityIn, e -> e.sendEquipmentBreakStatus(EquipmentSlot.MAINHAND));
+                    entityIn.fallDistance = 0.0F;
+
+                    if(!worldIn.isClient && worldIn instanceof ServerWorld)
+                    {
+                        ((ServerWorld)worldIn).spawnParticles(ParticleTypes.CLOUD, entityIn.lastRenderX, entityIn.lastRenderY, entityIn.lastRenderZ, 3, 0, 0, 0, (worldIn.random.nextFloat() - 0.5F));
+                    }
+                }
+            }
+
+            else if(((ServerPlayerEntity)entityIn).getStackInHand(Hand.OFF_HAND).getItem() == stack.getItem())
+            {
+                if(entityIn.fallDistance >= 3.0F)
+                {
+                    ((ServerPlayerEntity)entityIn).getStackInHand(Hand.OFF_HAND).damage(1, (ServerPlayerEntity)entityIn, e -> e.sendEquipmentBreakStatus(EquipmentSlot.OFFHAND));
+                    entityIn.fallDistance = 0.0F;
+
+                    if(!worldIn.isClient && worldIn instanceof ServerWorld)
+                    {
+                        ((ServerWorld)worldIn).spawnParticles(ParticleTypes.CLOUD, entityIn.lastRenderX, entityIn.lastRenderY, entityIn.lastRenderZ, 3, 0, 0, 0, (worldIn.random.nextFloat() - 0.5F));
+                    }
                 }
             }
         }
