992e49373290836805855224860ab6eaf6e5a86f
Kamil <34790800+Tiviacz1337@users.noreply.github.com>
2023-11-22 18:54:57 +0100

    ✨ Added 4 tiers of Cloud Boots - Iron, Gold, Diamond and Netherite

--- src/main/java/com/tiviacz/cloudboots/CloudBoots.java
+++ src/main/java/com/tiviacz/cloudboots/CloudBoots.java
@@ -2,6 +2,7 @@ package com.tiviacz.cloudboots;
 
 import net.fabricmc.api.ModInitializer;
 import net.fabricmc.loader.api.FabricLoader;
+import net.minecraft.item.ArmorMaterials;
 import net.minecraft.item.Item;
 import net.minecraft.item.ItemGroup;
 import net.minecraft.util.Identifier;
@@ -24,11 +25,19 @@ public class CloudBoots implements ModInitializer
     public static class ModItems
     {
         public static Item CLOUD_BOOTS;
+        public static Item IRON_CLOUD_BOOTS;
+        public static Item GOLD_CLOUD_BOOTS;
+        public static Item DIAMOND_CLOUD_BOOTS;
+        public static Item NETHERITE_CLOUD_BOOTS;
         public static Item GOLDEN_FEATHER;
 
         public static void init()
         {
-            CLOUD_BOOTS = Registry.register(Registry.ITEM, new Identifier(CloudBoots.MODID, "cloud_boots"), new CloudBootsItem(new Item.Settings().group(ItemGroup.COMBAT)));
+            CLOUD_BOOTS = Registry.register(Registry.ITEM, new Identifier(CloudBoots.MODID, "cloud_boots"), new CloudBootsItem(CloudBootsItem.CloudArmorMaterial.CLOUD, 0.15D, 4));
+            IRON_CLOUD_BOOTS = Registry.register(Registry.ITEM, new Identifier(CloudBoots.MODID, "iron_cloud_boots"), new CloudBootsItem(ArmorMaterials.IRON, 0.05D, 1));
+            GOLD_CLOUD_BOOTS = Registry.register(Registry.ITEM, new Identifier(CloudBoots.MODID, "gold_cloud_boots"), new CloudBootsItem(ArmorMaterials.GOLD, 0.10D, 2));
+            DIAMOND_CLOUD_BOOTS = Registry.register(Registry.ITEM, new Identifier(CloudBoots.MODID, "diamond_cloud_boots"), new CloudBootsItem(ArmorMaterials.DIAMOND, 0.15D, 3));
+            NETHERITE_CLOUD_BOOTS = Registry.register(Registry.ITEM, new Identifier(CloudBoots.MODID, "netherite_cloud_boots"), new CloudBootsItem(ArmorMaterials.NETHERITE, 0.20D, 4));
             GOLDEN_FEATHER = Registry.register(Registry.ITEM, new Identifier(CloudBoots.MODID, "golden_feather"), new GoldenFeatherItem(new Item.Settings().group(ItemGroup.MISC).maxCount(1).maxDamageIfAbsent(385)));
         }
     }
--- src/main/java/com/tiviacz/cloudboots/GoldenFeatherItem.java
+++ src/main/java/com/tiviacz/cloudboots/GoldenFeatherItem.java
@@ -1,15 +1,24 @@
 package com.tiviacz.cloudboots;
 
+import net.minecraft.client.item.TooltipContext;
 import net.minecraft.entity.Entity;
 import net.minecraft.entity.EquipmentSlot;
+import net.minecraft.entity.effect.StatusEffects;
 import net.minecraft.item.Item;
 import net.minecraft.item.ItemStack;
 import net.minecraft.item.Items;
 import net.minecraft.particle.ParticleTypes;
 import net.minecraft.server.network.ServerPlayerEntity;
 import net.minecraft.server.world.ServerWorld;
+import net.minecraft.text.MutableText;
+import net.minecraft.text.Text;
+import net.minecraft.text.TranslatableText;
+import net.minecraft.util.Formatting;
 import net.minecraft.util.Hand;
 import net.minecraft.world.World;
+import org.jetbrains.annotations.Nullable;
+
+import java.util.List;
 
 public class GoldenFeatherItem extends Item
 {
@@ -51,6 +60,13 @@ public class GoldenFeatherItem extends Item
                 }
             }
         }
+    }
+
+    @Override
+    public void appendTooltip(ItemStack stack, @Nullable World world, List<Text> tooltip, TooltipContext context)
+    {
+        super.appendTooltip(stack, world, tooltip, context);
+        tooltip.add(new TranslatableText("item.cloudboots.negates_fall_damage").formatted(Formatting.BLUE));
     }
 
     @Override
