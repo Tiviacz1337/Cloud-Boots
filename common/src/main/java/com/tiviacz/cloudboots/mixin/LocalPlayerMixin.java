4828ca1af2b382fb2a2053b3bd608a465d6b0adb
Kamil <34790800+Tiviacz1337@users.noreply.github.com>
2026-03-15 20:16:47 +0100

    🔧 Misc

--- common/src/main/java/com/tiviacz/cloudboots/mixin/LocalPlayerMixin.java
+++ common/src/main/java/com/tiviacz/cloudboots/mixin/LocalPlayerMixin.java
@@ -10,7 +10,7 @@ import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
 @Mixin(LocalPlayer.class)
 public class LocalPlayerMixin {
-    @Inject(method = {"playSound", "method_5783"}, at = @At(value = "HEAD"), cancellable = true, remap = false)
+    @Inject(method = {"playSound", "method_5783", "m_5496_"}, at = @At(value = "HEAD"), cancellable = true, remap = false)
     private void cancelBlockFallSound(SoundEvent sound, float volume, float pitch, CallbackInfo ci) {
         LocalPlayer localPlayer = (LocalPlayer)(Object)this;
         if((sound == localPlayer.getFallSounds().big() || sound == localPlayer.getFallSounds().small()) && CloudBootsItem.isCloudGearEquipped(localPlayer)) {
